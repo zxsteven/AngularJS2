be612d84da5c21c8e80b1c80d054ce427ed48d62
Steven Buxton <breathesteve@gmail.com>
2015-12-28 12:56:54 -0600

    Json files complete
    Added dynamic routes and linked to json files

--- script.js
+++ script.js
@@ -2,13 +2,31 @@ var app = angular.module("computer", ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider){
 	$routeProvider.
-	when('/main', {
-		templateUrl: 'main.html',
+	when('/about', {
+		templateUrl: 'about.html',
 		controller:'MainCtrl'
+	}).
+		when('/services', {
+		templateUrl: 'services.html',
+		controller:'ServicesCtrl'
+	}).
+		when('/contact', {
+		templateUrl: 'contact.html',
+		controller:'ContactCtrl'
 	}).
 	otherwise({redirectTo:'/main'})
 }]);
 
-.controller('MainCtrl', ['$scope', function($scope){
-	console.log($scope)
+.controller('MainCtrl', ['$scope', '$http' function($scope, $http){
+	$http.get('services.json').then(function(response){
+		$scope.services = response.data;
+}])
+.controller('ServicesCtrl', ['$scope', '$http', function($scope, $http){
+	$http.get('services.json').then(function(response){
+		$scope.services = response.data;
+	})
+}])
+.controller('ContactCtrl', ['$scope', '$http', function($scope, $http){
+	$http.get('locations.json').then(function(response){
+		$scope.locations = response.data;
 }])
